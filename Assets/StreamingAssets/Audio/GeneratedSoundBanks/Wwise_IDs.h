a815d6081904fbcc3a499ad76d305a82fc197439
fredKuko <frederic.kukovicic@gmail.com>
2023-11-30 21:22:38 +0100

    Add Wwise Projet and musics

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,98 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BACK = 1559875400U;
+        static const AkUniqueID HOVER = 3753593413U;
+        static const AkUniqueID PLAY_BOUNCE = 3465618802U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_GRAB = 17721092U;
+        static const AkUniqueID PLAY_HIGH_POWER_SMASH = 1822424553U;
+        static const AkUniqueID PLAY_LOW_POWER_SMASH = 746183055U;
+        static const AkUniqueID PLAY_MEDIUM_POWER_SMASH = 1190854492U;
+        static const AkUniqueID PLAY_MENU_MUSIC = 2228153899U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID PLAY_SCALESHOTA = 1976827765U;
+        static const AkUniqueID PLAY_SCALESHOTB = 1976827766U;
+        static const AkUniqueID PLAY_SIZEDOWN = 4097851697U;
+        static const AkUniqueID PLAY_SIZEUP = 598650966U;
+        static const AkUniqueID PLAY_THROW = 1977644338U;
+        static const AkUniqueID SELECT = 1432588725U;
+        static const AkUniqueID SET_SWITCH_GAMEPLAY = 1462254117U;
+        static const AkUniqueID SET_SWITCH_MENU = 1197906100U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CLEAN = 1754255532U;
+                static const AkUniqueID ELECTRICITY = 2917121896U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+        namespace MUSIC_SWITCH
+        {
+            static const AkUniqueID GROUP = 2724869341U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GAMEPLAY = 89505537U;
+                static const AkUniqueID MENU = 2607556080U;
+            } // namespace SWITCH
+        } // namespace MUSIC_SWITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SENSITIVITY_SCALE = 137696981U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_MUSIC = 2318586166U;
+        static const AkUniqueID MASTER_SFX = 1078167454U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
